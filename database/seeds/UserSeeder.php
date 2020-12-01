05faf592259d09aa9f166b73072edeca9057a91a
Acepmon <dtsogtbayar123@gmail.com>
2020-12-01 13:14:05 +0800

    UserSeeder added and test user

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user')->insert([
+            'user_id' => 'admin',
+            'user_pw' => Hash::make('password'),
+            'user_nm' => 'Administrator',
+            'email' => env('ADMIN_EMAIL', null),
+            'role_cd' => 'A10AA',
+            'user_sts_yn' => 'Y',
+        ]);
+    }
+}
