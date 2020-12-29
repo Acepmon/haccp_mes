e2012da4892340c84c5091bbf62663d39ef58240
Tsogtbayar <dtsogtbayar123@gmail.com>
2020-12-29 15:44:09 +0800

    dropdown test completed

--- resources/js/src/views/DropdownTest.vue
+++ resources/js/src/views/DropdownTest.vue
@@ -11,11 +11,16 @@
 
     <vs-divider></vs-divider>
 
-    <v-select v-model="item2" :options="items2" :value="item2" />
+    <v-select v-model="item2" :options="items2" />
+
+    <vs-divider></vs-divider>
+    {{ item3 }}
+    <v-select v-model="item3" :options="items3" :reduce="item => item.comm2_cd" label="comm2_nm" />
   </vx-card>
 </template>
 
 <script>
+import comm_cd from '@/services/comm_cd'
 export default {
   data () {
     return {
@@ -23,11 +28,17 @@ export default {
       items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
       item2: null,
       items2: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+      item3: null,
+      items3: []
     }
   },
 
   created() {
-    console.log("Dropdown Test created");
+    comm_cd.fetch({
+      'cd1': 'A10'
+    }).then((res) => {
+      this.$set(this, 'items3', res.data)
+    })
   },
 };
 </script>
