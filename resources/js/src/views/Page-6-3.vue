499fece41b1e5e2c4b74d7b439cfa4dc7163e1a2
Acepmon <dtsogtbayar123@gmail.com>
2021-01-11 19:43:55 +0800

    page-6-3 template completed

--- resources/js/src/views/Page-6-3.vue
+++ resources/js/src/views/Page-6-3.vue
@@ -1,7 +1,58 @@
 <template>
   <vx-card id="div-with-loading" class="vs-con-loading__container main-card">
     <app-control>
+      <template v-slot:filter>
+        <span class="flex items-center px-5">발생기간</span>
+        <flat-pickr
+          :config="configFromdateTimePicker"
+          v-model="from"
+          placeholder="시작일자"
+          @on-change="onFromChange"
+          class="control-field-dtm mx-1"
+        />
+        <span class="flex items-center px-2">~</span>
+        <flat-pickr
+          :config="configTodateTimePicker"
+          v-model="to"
+          placeholder="종료일자"
+          @on-change="onToChange"
+          class="control-field-dtm mx-1"
+        />
+        <span class="flex items-center px-5">CCP 장비</span>
+        <vs-input
+          class="control-field-sm"
+          v-model="lmt_up"
+        />
+      </template>
       <template v-slot:action>
+        <vs-button
+          @click="query()"
+          class="mx-1 mr-16"
+          color="primary"
+          type="border"
+          >{{ $t("Query") }}</vs-button
+        >
+        <vs-button
+          @click="addDialog()"
+          class="mx-1"
+          color="primary invisible"
+          type="border"
+          >{{ $t("Add") }}</vs-button
+        >
+        <vs-button
+          @click="saveDialog()"
+          class="mx-1"
+          color="primary invisible"
+          type="border"
+          >{{ $t("Save") }}</vs-button
+        >
+        <vs-button
+          @click="removeDialog()"
+          class="mx-1"
+          color="primary invisible"
+          type="border"
+          >{{ $t("Delete") }}</vs-button
+        >
         <vs-button
           @click="closeDialog()"
           class="mx-1"
@@ -14,7 +65,42 @@
 
     <vs-divider />
 
-    page-6-3
+    <app-form v-if="item['ccp_esc_data:device_id'] != null">
+      <app-form-group full>
+        <!-- chart here -->
+      </app-form-group>
+
+      <app-form-group full>
+        <template v-slot:label>발생사유</template>
+
+        <vs-input
+          class="control-field-lg"
+          readonly
+          v-model="item['ccp_esc_data:reason']"
+        />
+      </app-form-group>
+    </app-form>
+
+    <ag-grid-vue
+      ref="agGridTable"
+      :localeText="localeText"
+      rowSelection="single"
+      @selection-changed="handleSelected"
+      :gridOptions="gridOptions"
+      class="ag-theme-material w-100 my-4 ag-grid-table"
+      style="max-height: 100%;"
+      :columnDefs="columnDefs"
+      :defaultColDef="defaultColDef"
+      :rowData="itemsComp"
+      :pagination="true"
+      :paginationPageSize="paginationPageSize"
+      :suppressPaginationPanel="true">
+    </ag-grid-vue>
+
+    <vs-pagination
+      :total="totalPages"
+      :max="maxPageNumbers"
+      v-model="currentPage" />
   </vx-card>
 </template>
 
@@ -28,22 +114,100 @@ import AppControl from "@/views/ui-elements/AppControl";
 import AppForm from "@/views/ui-elements/AppForm";
 import AppFormGroup from "@/views/ui-elements/AppFormGroup";
 
+import { AgGridVue } from 'ag-grid-vue'
+import AG_GRID_LOCALE_KR from '@/views/ui-elements/ag-grid-table/agGridLocaleKr.js';;
+import '@sass/vuexy/extraComponents/agGridStyleOverride.scss'
+
+import flatPickr from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+import { Korean as KoreanLocale } from "flatpickr/dist/l10n/ko.js"
+
+import moment from 'moment';
+
 export default {
   name: 'page-6-3',
   components: {
     AppControl,
     AppForm,
-    AppFormGroup
+    AppFormGroup,
+    AgGridVue,
+    flatPickr
   },
 
   data () {
     return {
-      
+      format: "yyyy-MM-dd",
+      from: null,
+      to: null,
+      configFromdateTimePicker: {
+        maxDate: null,
+        locale: KoreanLocale,
+      },
+      configTodateTimePicker: {
+        minDate: null,
+        locale: KoreanLocale,
+      },
+      lmt_up: null,
+
+      item: {
+        'ccp_esc_data:device_id': null,
+        'ccp_esc_data:srt_dtm': null,
+        'ccp_esc_data:end_dtm': null,
+        'ccp_esc_data:reason': null,
+      },
+      items: [],
+      localeText: AG_GRID_LOCALE_KR,
+      maxPageNumbers: 7,
+      gridOptions: {
+        rowHeight: 40,
+        headerHeight: 40
+      },
+      gridApi: null,
+      defaultColDef: {
+        sortable: true,
+        editable: false,
+        resizable: true,
+        suppressMenu: false
+      },
+      columnDefs: [
+        { headerName: 'No', field: 'no', cellStyle: {textAlign: 'center'}, width: 50 },
+        { headerName: '발생일시', field: 'ccp_esc_data:srt_dtm', filter: false, width: 200 },
+        { headerName: '종료일시', field: 'ccp_esc_data:end_dtm', filter: false, width: 200 },
+        { headerName: 'CCP 장비', field: 'ccp_esc_data:device_id', filter: false, width: 200 },
+        { headerName: '상한값', field: 'ccp_limit:lmt_up', filter: false, width: 200 },
+        { headerName: '하한값', field: 'ccp_limit:lmt_dn', filter: false, width: 200 },
+        { headerName: '발생사유', field: 'ccp_esc_data:reason', filter: false, width: 200 },
+      ]
     }
   },
 
   computed: {
+    itemsComp: function () {
+      return this.items.map((item, index) => {
+        return {
+          'no': (index + 1),
+          ...item
+        } 
+      })
+    },
 
+    totalPages () {
+      if (this.gridApi) return this.gridApi.paginationGetTotalPages()
+      else return 0
+    },
+    paginationPageSize () {
+      if (this.gridApi) return this.gridApi.paginationGetPageSize()
+      else return 50
+    },
+    currentPage: {
+      get () {
+        if (this.gridApi) return this.gridApi.paginationGetCurrentPage() + 1
+        else return 1
+      },
+      set (val) {
+        this.gridApi.paginationGoToPage(val - 1)
+      }
+    },
   },
 
   methods: {
@@ -62,6 +226,64 @@ export default {
       }
     },
 
+    handleSelected () {
+      let rows = this.gridApi.getSelectedRows()
+
+      if (rows.length > 0) {
+        this.$set(this, 'item', $rows[0]);
+      }
+    },
+
+    onFromChange(selectedDates, dateStr, instance) {
+      this.$set(this.configTodateTimePicker, "minDate", dateStr);
+    },
+
+    onToChange(selectedDates, dateStr, instance) {
+      this.$set(this.configFromdateTimePicker, "maxDate", dateStr);
+    },
+
+    query () {
+      this.spinner();
+
+      let search_params = {};
+
+      if (this.from != null) {
+        search_params['from'] = this.from;
+      }
+
+      if (this.to != null) {
+        search_params['to'] = this.to;
+      }
+
+      if (this.lmt_up != null) {
+        search_params['lmt_up'] = this.lmt_up;
+      }
+
+      ccp_esc_data
+        .fetch({
+          sort: 'SRT_DTM',
+          order: 'DESC',
+          limit: -1,
+          with: 'ccp_limit',
+          ...search_params,
+        })
+        .then((res) => {
+          this.spinner(false);
+          this.items = res.data.data;
+        })
+        .catch((err) => {
+          this.spinner(false);
+          this.$vs.notify({
+            title: this.$t("Error"),
+            position: "top-right",
+            color: "warning",
+            iconPack: "feather",
+            icon: "icon-alert-circle",
+            text: err.response.data.message,
+          });
+        });
+    },
+
     closeDialog() {
       this.$vs.dialog({
         type: "confirm",
@@ -76,11 +298,13 @@ export default {
   },
 
   mounted () {
-
+    this.gridApi = this.gridOptions.api
   },
 
   created () {
-
+    setTimeout(() => {
+      this.query();
+    }, 200);
   }
 }
 </script>
