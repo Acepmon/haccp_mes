905fd4dbb63049da7708ea37d79c2f193bc1c8bd
Acepmon <dtsogtbayar123@gmail.com>
2021-01-11 16:37:34 +0800

    default parameter value for device_id on ccp_data added

--- app/Http/Controllers/API/HaccpMonitorController.php
+++ app/Http/Controllers/API/HaccpMonitorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\CcpData;
+use App\CommCd;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CcpDataResource;
 use Illuminate\Http\Request;
@@ -10,72 +11,11 @@ use Illuminate\Support\Facades\DB;
 
 class HaccpMonitorController extends Controller
 {
-    // public function ccp_data(Request $request)
-    // {
-    //     $items = CcpData::query()->select('CCP_SEQ', 'DEVICE_ID', 'DATA', 'REG_DTM');
-
-    //     $limit = $request->input('limit', 15);
-    //     $sort = $request->input('sort', 'REG_DTM');
-    //     $order = $request->input('order', 'DESC');
-    //     $device_id = $request->input('device_id');
-    //     $reg_dtm = $request->input('reg_dtm');
-    //     $from = $request->input('from');
-
-    //     if ($request->has('device_id')) {
-    //         $items = $items->whereIn('DEVICE_ID', array_filter(explode(',', $device_id)));
-    //     }
-
-    //     if ($request->has('reg_dtm')) {
-    //         $items = $items->where('REG_DTM', 'LIKE', $reg_dtm . '%');
-    //     }
-
-    //     if ($request->has('from')) {
-    //         $items = $items->whereDate('REG_DTM', '>=', now()->parse($from)->format('YmdHis'));
-    //     }
-
-    //     if ($request->has('groupBy')) {
-    //         $items = $items->groupBy('device_id');
-    //     }
-
-    //     if ($limit == -1) {
-    //         $items = $items->orderBy($sort, $order)->get();
-    //     } else {
-    //         $items = $items->orderBy($sort, $order)->paginate($limit);
-    //     }
-
-    //     if ($request->has('stats')) {
-    //         $stats = CcpData::select(DB::raw('MIN(CAST(DATA AS DECIMAL(10,2))) MIN, MAX(CAST(DATA AS DECIMAL(10,2))) MAX, AVG(CAST(DATA AS DECIMAL(10,2))) AVG'));
-
-    //         if ($request->has('device_id')) {
-    //             $stats = $stats->where('DEVICE_ID', $device_id);
-    //         }
-
-    //         if ($request->has('device_id')) {
-    //             $stats = $stats->where('REG_DTM', 'LIKE', $reg_dtm . '%');
-    //         }
-
-    //         $stats = $stats->first();    
-    //         $items = $items->map(function ($item) use ($stats) {
-    //             return (object) [
-    //                 'CCP_SEQ' => $item->CCP_SEQ,
-    //                 'DEVICE_ID' => $item->DEVICE_ID,
-    //                 'DATA' => $item->DATA,
-    //                 'REG_DTM' => $item->REG_DTM,
-    //                 'MIN' => $stats->MIN,
-    //                 'MAX' => $stats->MAX,
-    //                 'AVG' => $stats->AVG,
-    //             ];
-    //         });
-    //     }
-
-    //     return CcpDataResource::collection($items);
-    // }
-
     public function ccp_data(Request $request)
     {
         $sort = $request->input('sort', 'DEVICE');
         $order = $request->input('order', 'ASC');
-        $device_id = $request->input('device_id');
+        $device_id = $request->input('device_id', implode(',', CommCd::where('COMM1_CD', 'C00')->whereNotIn('COMM2_CD', ['$$'])->get()->pluck('COMM2_CD')->toArray()));
         $reg_dtm = $request->input('reg_dtm');
         $from = $request->input('from');
 
