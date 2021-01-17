f76eb90015e3bb7fc948757e6dfddf5e8fd514ca
Acepmon <dtsogtbayar123@gmail.com>
2021-01-17 15:05:36 +0800

    ccp_cd_info migration, model, events and resource file created

---
+++ app/Events/CcpCdInfoCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\CcpCdInfo;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CcpCdInfoCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $ccpCdInfo;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(CcpCdInfo $ccpCdInfo)
+    {
+        $this->ccpCdInfo = $ccpCdInfo;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
